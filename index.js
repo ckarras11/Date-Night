23ef086254624f3610e421559d7890b13b0e11c4
Christopher Karras <ckarras11@gmail.com>
2017-08-26 21:51:56 -0400

    broke down some of the larger functions into smaller ones, minimized global  variables

--- index.js
+++ index.js
@@ -1,208 +1,213 @@
-let clientId = "SSYNT1OJ1S0G44S211LRDRBAY530BAYWZYQXCXDUDN4DYAYK"
-let clientSecret = "SKWPCMJ3315543VFGOUCQD5XEKKA1NKDJ2GRURT5EURRXUQA"
-let coordinates = [];
 let map;
-navigator.geolocation.getCurrentPosition(success, failure);
-let start = {};
-
+let info = {
+	clientId: 'SSYNT1OJ1S0G44S211LRDRBAY530BAYWZYQXCXDUDN4DYAYK',
+	start: {},
+	coordinates: []
+	}
       
-//This function is the intital api call
-//41.715624,-70.032247
-function getResponse(section, query){
-	console.log('load');
-	const URL = `https://api.foursquare.com/v2/venues/explore?client_id=${clientId}
-															 &client_secret=${clientSecret}
-															 &ll=${start.lat},${start.lng}
+// This function is the intital api call
+function getResponse(section, query) {
+	const URL = `https://api.foursquare.com/v2/venues/explore?client_id=${info.clientId}
+															 &client_secret=SKWPCMJ3315543VFGOUCQD5XEKKA1NKDJ2GRURT5EURRXUQA
+															 &ll=${info.start.lat},${info.start.lng}
 															 &query=${query}
 															 &section=${section}
 															 &limit=100
 															 &radius=40000&v=20170323`;
 	$.ajax({
-		method: "GET",
+		method: 'GET',
 		url: URL,
-		success: function(data){
+		success: function(data) {
 			createList(data);
+			createLatLng(data);
+			initMap();
+			addMarkers();
+			searchAgain();
 		}
 
 	});
 };
 
-//Handles search and passes value in search bar to query for api call
-//The section arg is undefined because it will override a query
+$(function handleStart(document) {
+	handleQuery();
+	handleButtons();
+});
 
-$(function handleQuery(document){
-	$('.search').on('click', '.submit', function(e){
+// Handles search and passes value in search bar to query for api call
+// The section arg is undefined because it will override a query
+
+function handleQuery() {
+	navigator.geolocation.getCurrentPosition(success, failure);
+	$('.search').on('click', '.submit', function(e) {
 		e.preventDefault();
 		let query = $('.searchbar').val();
 		displayResults();
 		getResponse(undefined, query);
 		
 	});
-});
+};
 
-//Handles food button and passes 'food' to the section arg
+// Handles food button and passes 'food' to the section arg
 
-$(function handleButtons(document){
-	$('#buttons').on('click', '#food', function(){
+function handleButtons() {
+	$('#buttons').on('click', '#food', function() {
 		displayResults();
 		let section = 'food';
 		getResponse(section);
 	});
-	$('#buttons').on('click', '#drinks', function(){
+	$('#buttons').on('click', '#drinks', function() {
 		displayResults();
 		let section = 'drinks';
 		getResponse(section);
 	});
-	$('#buttons').on('click', '#entertainment', function(){
+	$('#buttons').on('click', '#entertainment', function() {
 		displayResults();
 		let section = 'arts';
 		getResponse(section);
 	});
-});
+};
 
-function createList(data){
-	console.log(data);
-			//This creates list element for each item returned
-			let list = "";
-			data.response.groups[0].items.forEach(item => {
-				let i = data.response.groups[0].items.indexOf(item) + 1;
+// This creates list element for each item returned
 
-				let address = "";
-				if (item.venue.location.address !== undefined){
-					address = item.venue.location.address;
-				}
-				let city = "";
-				if (item.venue.location.city !== undefined){
-					city = item.venue.location.city;
-				}
-				let state = "";
-				if (item.venue.location.state !== undefined){
-					state = item.venue.location.state;
-				}
-				let postalCode = "";
-				if (item.venue.location.postalCode !== undefined){
-					postalCode = item.venue.location.postalCode;
-				}
-
-				let rating = "";
-				if (item.venue.rating !== undefined){
-					rating = `
-					<div class="rating">
-						<div class="score" style="background-color: #${item.venue.ratingColor};">
-							${item.venue.rating}
-						</div>
-					</div>`
-				}
-				list = list.concat(`<li class="itemResult">
-										<div class="info">
-											<div class="venueName">
-												${i}) ${item.venue.name}
-											</div>
-											<div class="location">
-												${address} ${city}, ${state} ${postalCode}
-											</div>
-											<div class="moreInfo">
-												<a href="https://foursquare.com/v/${item.venue.id}?ref=${clientId}" target="blank">More Info</a>
-											</div>
-										</div>
-										${rating}
-									</li>`)
-			})
-			//This creates a lat long object for each item returned and pushes to array
-			data.response.groups[0].items.forEach(item => {
-				let i = data.response.groups[0].items.indexOf(item) + 1;
-				let obj = {lat: item.venue.location.lat,
-						   lng: item.venue.location.lng,
-						   name: item.venue.name,
-						   index: i};
-				
-				coordinates.push(obj);
-			});
-			initMap();
-			addMarkers();
-			
-
-			
-
-        	//Adds a ul element with the list created in for loop
-			$('#results').append(`<ul id="resultsList">
-									${list}
-								  </ul>`);
-			
+function createList(data){	
+	let list = "";
+	data.response.groups[0].items.forEach(item => {
+		let i = data.response.groups[0].items.indexOf(item) + 1;
+		// Checks to make sure there is data for each location to prevent displaying undefined
+		let address = "";
+		if (item.venue.location.address !== undefined) {
+			address = item.venue.location.address;
+		}
+		let city = "";
+		if (item.venue.location.city !== undefined) {
+			city = item.venue.location.city;
+		}
+		let state = "";
+		if (item.venue.location.state !== undefined) {
+			state = item.venue.location.state;
+		}
+		let postalCode = "";
+		if (item.venue.location.postalCode !== undefined) {
+			postalCode = item.venue.location.postalCode;
+		}
+		let rating = "";
+		// Creates the rating div for each item
+		if (item.venue.rating !== undefined) {
+			rating = `
+			<div class="rating">
+				<div class="score" style="background-color: #${item.venue.ratingColor};">
+					${item.venue.rating}
+				</div>
+			</div>`
+		}
+		// Creates the result list for each item
+		list = list.concat(`<li class="itemResult">
+								<div class="info">
+									<div class="venueName">
+										${i}) ${item.venue.name}
+									</div>
+									<div class="location">
+										${address} ${city}, ${state} ${postalCode}
+									</div>
+									<div class="moreInfo">
+										<a href="https://foursquare.com/v/${item.venue.id}?ref=${info.clientId}" target="blank">More Info</a>
+									</div>
+								</div>
+								${rating}
+							</li>`)
+	})
+	// Adds a ul element with the list created in for loop
+	$('#results').append(`<ul id="resultsList">
+							${list}
+						  </ul>`);		
 }
 
+// This creates a lat long object for each item returned and pushes to array
 
-//Creates map in #map div with map centered on lat long
+function createLatLng(data) {
+	data.response.groups[0].items.forEach(item => {
+		let i = data.response.groups[0].items.indexOf(item) + 1;
+		let obj = {lat: item.venue.location.lat,
+				   lng: item.venue.location.lng,
+				   name: item.venue.name,
+				   index: i};		
+		info.coordinates.push(obj);
+	});
+}
+
+// Creates map in #map div with map centered on lat long
 
 function initMap() {
-	console.log('initMap')
 	map = new google.maps.Map(document.getElementById('map'), {
   		zoom: 12,
-  		center: new google.maps.LatLng(start.lat,start.lng),
+  		center: new google.maps.LatLng(info.start.lat,info.start.lng),
   		mapTypeId: 'roadmap'
 		});
 }
 
-//Adds markers to the map from the coordinates array (data from foursquare lat long)
+// Adds markers to the map from the coordinates array (data from foursquare lat long)
 
-function addMarkers(){
-	for (let i = 0; i < coordinates.length; i++) {
-        let coords = coordinates[i];
+function addMarkers() {
+	for (let i = 0; i < info.coordinates.length; i++) {
+        let coords = info.coordinates[i];
         let latLng = new google.maps.LatLng(coords.lat,coords.lng);
         let marker = new google.maps.Marker({
             								position: latLng,
             								map: map,
             								title: `${coords.name}`
           									});
-        //Creates info window showing name and index
+        // Creates info window for each marker showing name and index number
 
         let infoWindow = new google.maps.InfoWindow({
         	content: `${coords.index}) ${coords.name}`
         });
-        marker.addListener('click', function(){
+        marker.addListener('click', function() {
         	infoWindow.open(map, marker);
         });
     };
 }
 
-//Handles Search again button on the results page 
+// Handles Search again button on the results page 
 
-$(function searchAgain(document){
-	$("#nav").on('click', '.searchAgain', function(){
-		$('#homepage').removeClass("js-hide-display");
-		$('#container').addClass("js-hide-display");
-		$('.mainContent').addClass("js-hide-display");
-		$('#nav').addClass("js-hide-display");
+function searchAgain(){
+	$('#nav').on('click', '.searchAgain', function(){
+		$('#homepage').removeClass('js-hide-display');
+		$('#container').addClass('js-hide-display');
+		$('.mainContent').addClass('js-hide-display');
+		$('#nav').addClass('js-hide-display');
 		removeMarkers();
-		$(".searchbar").val("");
+		$('.searchbar').val('');
 	})
-});
+};
 
 // Removes markers from map for new search
 
-function removeMarkers(){
-	coordinates = [];
+function removeMarkers() {
+	info.coordinates = [];
 };
 
-//Adds and removes js-hide-display to display results
+// Adds and removes js-hide-display to display results
 
-function displayResults(){
+function displayResults() {
 	$('#results').html('');
-	$('#homepage').addClass("js-hide-display");
-	$('#container').removeClass("js-hide-display");
-	$('.mainContent').removeClass("js-hide-display");
-	$('#nav').removeClass("js-hide-display");
-	$('#map').removeClass("js-hide-display");
+	$('#homepage').addClass('js-hide-display');
+	$('#container').removeClass('js-hide-display');
+	$('.mainContent').removeClass('js-hide-display');
+	$('#nav').removeClass('js-hide-display');
+	$('#map').removeClass('js-hide-display');
 }
 
-function success(position){
+// Sets users location when geolocation is allowed by user
+
+function success(position) {
 	let lat = position.coords.latitude;
 	let lng = position.coords.longitude;
-	start.lat = lat;
-	start.lng = lng;
-	console.log(lat,lng)
+	info.start.lat = lat;
+	info.start.lng = lng;
 }
 
-function failure(){
-	alert('Geolocation not available')
+// Alerts user that geolocation was blocked
+
+function failure() {
+	alert('Location services blocked, please allow to view content')
 }
